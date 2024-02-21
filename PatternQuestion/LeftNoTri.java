f114e9fe8801eab7fdc9c434b5116ff978f2d89d
Mansi-Raj <mansi.raj.jnv@gmail.com>
2024-02-21 10:49:28 +0530

    add codes

---
+++ PatternQuestion/LeftNoTri.java
@@ -0,0 +1,16 @@
+package DataStructureJava.PatternQuestion;
+
+import java.util.*;
+
+public class LeftNoTri {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
